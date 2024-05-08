756d5da9e1c46753f6da369e5fdd56832f20300e
mofris <mochammadfaris001@gmail.com>
2024-05-08 23:41:31 +0700

    setup project api

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const userRouter = require("./app/api/users/router");
 
-var app = express();
+const URL = "/api/v1";
 
-app.use(logger('dev'));
+const app = express();
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get("/", function (req, res) {
+  res.json({
+    message: "Welcome to Server Future PT. Citra Borneo Indah Jobs",
+    version: "1.0.0",
+    author: "Mochammad Faris",
+    contact: "loveinsemarang@gmail.com",
+  });
+});
+
+app.use(`${URL}`, userRouter);
 
 module.exports = app;
