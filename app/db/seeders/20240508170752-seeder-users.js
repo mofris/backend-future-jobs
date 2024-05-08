d049636a91e9495b81096f06c520b169a2d36faa
mofris <mochammadfaris001@gmail.com>
2024-05-09 00:15:07 +0700

    setup sequelize dan mysql2

---
+++ app/db/seeders/20240508170752-seeder-users.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "Users",
+      [
+        {
+          name: "Mochammad Faris",
+          username: "faris",
+          password: "dyhisd",
+          is_active: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Users", null, {});
+  },
+};
